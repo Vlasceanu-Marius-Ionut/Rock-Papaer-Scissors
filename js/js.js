07fbd4ad342e26b859056f674a8b0ddf3bec1a5b
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-10-14 16:48:29 +0300

    update

--- js/js.js
+++ js/js.js
@@ -12,3 +12,26 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
 //DOM_CASH
+
+function getComputerChoice() {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3); //never go over 3;
+
+  return choices(randomNumber);
+}
+
+function game(userChoice) {
+  console.log("sss" + userChoice);
+}
+function main() {
+  rock_div.addEventListener("click", function () {
+    game("r");
+  });
+  rock_div.addEventListener("click", function () {
+    game("p");
+  });
+  rock_div.addEventListener("click", function () {
+    game("s");
+  });
+}
+main();
