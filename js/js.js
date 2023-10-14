57d582830175b6d1ec376807509848ff9bbcd65c
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-10-14 16:41:32 +0300

    update

--- js/js.js
+++ js/js.js
@@ -1,6 +1,14 @@
 "use strict";
 
+//AVG VAR
 const userScore = 0;
 const computerScore = 0;
+//DOM
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".res");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
+//DOM_CASH
