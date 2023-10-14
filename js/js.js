b3cd55654e96e9a1171bdf8b64bba08fcf5be619
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-10-14 17:11:26 +0300

    update

--- js/js.js
+++ js/js.js
@@ -13,24 +13,56 @@ const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
 //DOM_CASH
 
+//play the game
 function getComputerChoice() {
   const choices = ["r", "p", "s"];
   const randomNumber = Math.floor(Math.random() * 3); //never go over 3;
 
-  return choices(randomNumber);
+  return choices[randomNumber];
+}
+
+function wins() {
+  console.log("WIN");
+}
+function lose() {
+  console.log("LOST");
+}
+function draw() {
+  console.log("DRAW");
 }
 
 function game(userChoice) {
-  console.log("sss" + userChoice);
+  const computerChoice = getComputerChoice();
+  const result = userChoice + computerChoice;
+
+  switch (result) {
+    case "rs":
+    case "pr":
+    case "sp":
+      wins();
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose();
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw();
+      break;
+  }
 }
+
+//event listeners
 function main() {
   rock_div.addEventListener("click", function () {
     game("r");
   });
-  rock_div.addEventListener("click", function () {
+  paper_div.addEventListener("click", function () {
     game("p");
   });
-  rock_div.addEventListener("click", function () {
+  scissor_div.addEventListener("click", function () {
     game("s");
   });
 }
