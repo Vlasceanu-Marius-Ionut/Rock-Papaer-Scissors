dc34030e9c78916356ff51d2b551833488a857ab
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2023-10-14 16:39:41 +0300

    update

--- js/js.js
+++ js/js.js
@@ -1,1 +1,6 @@
 "use strict";
+
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
